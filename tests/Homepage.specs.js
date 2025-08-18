474b77229820bef1013962f52d14d2c20f37dfbc
Aerry Asmani <aerryasmani@gmail.com>
2025-08-18 15:36:43 +0800

    Remove sample apps Add script to check img

--- tests/Homepage.specs.js
+++ tests/Homepage.specs.js
@@ -1,6 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('Go to homepage', async ({ page }) => {
+test('Homepage is Present', async ({ page }) => {
   await page.goto('/'); // no need to repeat baseURL
   await expect(page).toHaveTitle(/Sedekah Je/); 
+});
+
+test('Sedekahje Logo is Present', async ({ page }) => {
+  await page.goto('/'); // 
+  
+  // locate the image (by alt text, id, or class)
+  const logo = page.locator('img[alt="Logo"]');
+
+  // check it's present in the DOM
+  await expect(logo).toHaveCount(1);
+
+  // check it's visible to the user
+  await expect(logo).toBeVisible();
+
 });
