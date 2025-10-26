27d4342f8455976340f828283151d3a059252315
Aerry Asmani <aerryasmani@gmail.com>
2025-10-26 22:56:00 +0800

    Added imag verification function and updated test names for clarity

--- tests/TestSuite/homepage.spec.js
+++ tests/TestSuite/homepage.spec.js
@@ -9,6 +9,23 @@ async function VerifyPageTitle (page) {
   await expect(page).toHaveTitle(PageTitle);
 }
 
+async function VerifyLogoVisibility (page) {
+
+const LogoLocator = page.locator('img[alt="Masjid"]');
+
+// ✅ Check visibility
+await expect(LogoLocator).toBeVisible({ timeout: 10000 });
+
+// ✅ Check attributes
+await expect(LogoLocator).toHaveAttribute('alt', 'Masjid');
+await expect(LogoLocator).toHaveAttribute('src', '/masjid.svg');
+await expect(LogoLocator).toHaveAttribute('width', '100');
+await expect(LogoLocator).toHaveAttribute('height', '100');
+await expect(LogoLocator).toHaveAttribute('loading', 'lazy');
+
+
+}
+
 // Setup
 test.beforeEach(async ({ page }) => {
   await page.goto(BaseURL);
@@ -17,6 +34,12 @@ test.beforeEach(async ({ page }) => {
 
 //------- Test Cases ------//
 
-test('CF-001 | Homepage displays correct title', async ({ page }) => {
+test('CF-001 | Homepage | Title displays correctly on page load', async ({ page }) => {
   await VerifyPageTitle(page);
+});
+
+
+test('CF-002 | Homepage | Should display logo correctly on page load', async ({ page }) => {
+  await VerifyLogoVisibility(page);
+
 });
