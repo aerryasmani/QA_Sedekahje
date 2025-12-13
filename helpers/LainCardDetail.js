cdfdeb9c544111c3944c83c2b059846d4c07ed65
Aerry Asmani <aerryasmani@gmail.com>
2025-12-13 23:21:45 +0800

    Added Test Cases Added Helper Function

--- helpers/LainCardDetail.js
+++ helpers/LainCardDetail.js
@@ -10,3 +10,41 @@ export async function VerifyLainButton(page){
  const filterResultText = page.getByText('Jumlah Hasil Tapisan');
  await expect(filterResultText).toBeVisible();
 }
+
+export async function VerifyLainCard_Result(page){
+ const resultCard_Lain = page.locator('div.rounded-lg.bg-card').first();
+ await expect(resultCard_Lain).toBeVisible();
+
+ const cardLogo = resultCard_Lain.getByRole('img', { name: 'category logo' });
+ await expect(cardLogo).toBeVisible();
+
+ const cardTitle = resultCard_Lain.locator('h3.text-lg.font-semibold');
+ await expect(page.getByText('AJK Tanah Kubur Peradong')).toBeVisible();
+
+ await page.mouse.wheel(0, 1000);
+
+ const DownloadButton = resultCard_Lain.locator('button', { has: page.locator('svg.lucide-download') });
+ await expect(DownloadButton).toBeVisible();
+
+ const ShareButton = resultCard_Lain.locator('button', { has: page.locator('svg.lucide-share2') });
+ await expect(ShareButton).toBeVisible();
+}
+
+export async function VerifyCardLain_PetaButton(page){
+  const resultCard_Surau = page.locator('div.rounded-lg.bg-card').first();
+  //const cardTitle = resultCard_Surau.locator('h3.text-lg.font-semibold');
+  const CardExpand= page.locator(".grid > div").first()
+  
+  await expect(page.getByText('AJK Tanah Kubur Peradong')).toBeVisible();
+  // Target the outermost div with all card styling
+  await CardExpand.click(page);
+  await expect(page.getByText(/AJK Tanah Kubur Peradong/i)).toBeVisible();
+  await expect(page.getByText(/Kota Bharu, Kelantan/i)).toBeVisible();
+  await resultCard_Surau.click();
+
+  await expect(resultCard_Surau).toHaveText('AJK Tanah Kubur PeradongKota Bharu, KelantanKongsi')
+
+  const PetaButton = page.getByRole('button',{name:'Peta'});
+  await expect(PetaButton).toBeVisible();
+  await PetaButton.click();
+}
--- tests/TestSuite/Lain_detailpage.spec.js
+++ tests/TestSuite/Lain_detailpage.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import {VerifyModalPopup,VerifyPageTitle,VerifyLogoVisibility,VerifyToogleButton,VerifyOrgButton,VerifyDropdown,DropdownFunctionality,VerifySearchbar,VerifySearchbar_Result,} from '../../helpers/homepage';
 import {VerifySurauButton,VerifyCard_Result,VerifyCard_PetaButton,VerifyFooter_BrandSection,VerifyFooter_Socmedia} from '../../helpers/SurauCardDetail';
-import {VerifyLainButton} from '../../helpers/LainCardDetail';
+import {VerifyLainButton,VerifyLainCard_Result,VerifyCardLain_PetaButton} from '../../helpers/LainCardDetail';
 
 const BaseURL = 'https://sedekah.je/';
 const PageTitle = 'Sedekah Je - Platform Sedekah QR Malaysia';
@@ -12,6 +12,20 @@ test.beforeEach(async ({ page }) => {
   await VerifyModalPopup(page);
 });
 
-test('CF-01 | Lain Detail Page | Title displays correctly on page load', async ({ page }) => {
-  await VerifyPageTitle(page);
+test('CF-018 | Lain Detail Page |  Lain-lain button is present and visible', async ({ page }) => {
+  await VerifyLainButton(page);
+  await VerifyLainCard_Result(page);
+  await VerifyCardLain_PetaButton(page);
+});
+
+test('CF-019 | Lain Detail Page | Lain Detail Card is present and visible', async ({ page }) => {
+  await VerifyLainButton(page);
+  await VerifyLainCard_Result(page);
+  await VerifyCardLain_PetaButton(page);
+});
+
+test('CF-020 | Lain Detail Page | Peta button is present and visible in the detail page', async ({ page }) => {
+  await VerifyLainButton(page);
+  await VerifyLainCard_Result(page);
+  await VerifyCardLain_PetaButton(page);
 });
