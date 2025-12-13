c32b7f12efdb69430514057488a1ea4e2dd1b2b9
Aerry Asmani <aerryasmani@gmail.com>
2025-12-13 23:47:36 +0800

    Modify helpers file

--- tests/TestSuite/Lain_detailpage.spec.js
+++ tests/TestSuite/Lain_detailpage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import {VerifyModalPopup,VerifyPageTitle,VerifyLogoVisibility,VerifyToogleButton,VerifyOrgButton,VerifyDropdown,DropdownFunctionality,VerifySearchbar,VerifySearchbar_Result,} from '../../helpers/homepage';
-import {VerifySurauButton,VerifyCard_Result,VerifyCard_PetaButton,VerifyFooter_BrandSection,VerifyFooter_Socmedia} from '../../helpers/SurauCardDetail';
+import {VerifySurauButton,VerifyCard_Result,VerifyCard_PetaButton,VerifyFooter_BrandSection,VerifyFooter_Socmedia,VerifyCard_GetDoa} from '../../helpers/SurauCardDetail';
 import {VerifyLainButton,VerifyLainCard_Result,VerifyCardLain_PetaButton} from '../../helpers/LainCardDetail';
 
 const BaseURL = 'https://sedekah.je/';
@@ -28,4 +28,35 @@ test('CF-020 | Lain Detail Page | Peta button is present and visible in the deta
   await VerifyLainButton(page);
   await VerifyLainCard_Result(page);
   await VerifyCardLain_PetaButton(page);
+});
+
+test ('CF - 021 | Lain Detail Page | Doa Section is present',async ({page}) => {
+  await VerifySurauButton(page);
+  await VerifyCard_Result(page);
+  await VerifyCard_PetaButton(page);
+  await VerifyCard_GetDoa(page);
+
+});
+
+test ('CF - 022 | Lain Detail Page | Footer Section is present',async ({page}) => {
+  await VerifySurauButton(page);
+  await VerifyCard_Result(page);
+  await VerifyCard_PetaButton(page);
+  await VerifyFooter_BrandSection(page);
+  await VerifyFooter_Socmedia (page);
+});
+
+test('CF - 023 | Footer Social Media is present', async ({page}) => {
+  await VerifySurau_Flow(page);
+  await VerifyFooter_Socmedia(page);
+});
+
+test('CF - 024 | Footer Rujukan Links are present', async ({page}) => {
+  await VerifySurau_Flow(page);
+  await VerifyFooter_RujukanLinks(page);
+});
+
+test('CF - 025 | Footer Project Komuniti Links are present', async ({page}) => {
+  await VerifySurau_Flow(page);
+  await VerifyFooter_ProjectKomuniti(page);
 });
